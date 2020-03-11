2b4d57569cebd61e6f591f97b63ea25c0a01547a
replacem3nts <donny.landis@gmail.com>
2020-03-11 19:01:50 -0400

    updates lab with all tests passing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require "pry"
+class School
+    attr_reader :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade_no)
+        @roster[grade_no] ? @roster[grade_no] << student : @roster[grade_no] = [student]
+    end
+
+    def grade(grade_no)
+        @roster[grade_no]
+    end
+
+    def sort
+        @roster.each_value do |student_array|
+            student_array.sort!
+        end
+    end
+end
